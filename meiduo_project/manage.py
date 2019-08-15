ce7854ef24cfc25bf58b737ee988caa2efe8dbc9
mingzitian <mingzitian@163.com>
2019-08-15 19:30:00 +0800

    配置文件 创建注册应用

--- meiduo_project/manage.py
+++ meiduo_project/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_project.settings.dev")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meiduo_project.settings.prod")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
